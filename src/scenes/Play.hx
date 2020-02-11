27a75bef10452d0e124b14e234418b1ec16225bc
Will Blanton <will.blanton.804@gmail.com>
2020-02-10 21:24:33 -0500

    some organization

--- src/scenes/Play.hx
+++ src/scenes/Play.hx
@@ -1,8 +1,6 @@
 package scenes;
 
-import openfl.Lib;
 import zero.utilities.Color;
-import objects.Tilemap;
 import openfl.display.Sprite;
 using objects.GameObject3D;
 
@@ -11,21 +9,23 @@ class Play extends Scene {
 	public function new() {
 		super();
 		var bg = new Sprite();
-		bg.fill_rect(Color.PICO_8_WHITE, 0, 0, Game.width, Game.height);
-		for (i in 0...256) bg.fill_rect(Color.PICO_8_GREEN, Game.width.get_random(), Game.height.get_random(), 4 * i % 3, 4 * i % 3);
-		for (i in 0...256) bg.fill_rect(Color.PICO_8_ORANGE, Game.width.get_random(), Game.height.get_random(), 8 * i % 3, 8 * i % 3);
-		for (i in 0...100) bg.add(new Tree(Game.width.get_random(), Game.height.get_random()));
-		addChild(bg);
-		var car = new Car(Game.width/2, Game.height/2);
-		bg.addChild(car);
-		((?dt) -> bg.sort3D()).listen('update');
-		((?_) -> {
-			bg.center(car.x, car.y);
-			var target_angle = -car.angle - 90;
-			while ((bg.rotation - target_angle).abs() > 180) target_angle += bg.rotation > target_angle ? 360 : -360;
-			bg.rotation += (target_angle - bg.rotation) * 0.025;
-		}).listen('update');
+		bg.fill_rect(Color.PICO_8_PEACH, 0, 0, Game.width, Game.height);
 		bg.set_scale(2);
+		bg.rotation = 45;
+		
+		for (i in 0...128) bg.add(new Tree(Game.width.get_random(), Game.height.get_random()));
+		var car = new Car(Game.width/2, Game.height/2);
+		bg.add(car);
+
+		((?_) -> {
+			bg.sort3D();
+			bg.center(car.x, car.y);
+			//var target_angle = -car.angle - 90;
+			//while ((bg.rotation - target_angle).abs() > 180) target_angle += bg.rotation > target_angle ? 360 : -360;
+			//bg.rotation += (target_angle - bg.rotation) * 0.025;
+		}).listen('update');
+		
+		addChild(bg);
 		addChild(new util.FPS(10, 10, 0x808090));
 	}
 
